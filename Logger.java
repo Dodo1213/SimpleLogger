031c23be88cd2bc935ef7072066322492ff3b0cb
NastyOOF <46837478+NastyOOF@users.noreply.github.com>
2021-01-07 01:48:52 +0100

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -1,16 +1,14 @@
-package de.nasty.main.logger;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
 /*
  *
  *  * Class created by Nasty / Ron S.
  *  * 07.01.21, 01:08
  *
  */
+
+package de.nasty.main.logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
     private static final String RESET = "\u001B[0m";
@@ -24,22 +22,21 @@ public class Logger {
     private static final String WHITE = "\u001B[37m";
 
     public static void send(Types type, String message) {
-        String mainPrefix = "[NAME]";
-      //  SimpleDateFormat format = new SimpleDateFormat("[hh:mm:ss] ");
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("[hh:mm:ss] ");
+        String mainPrefix = "[HUE-BOT]";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss");
         String date = dateTimeFormatter.format(LocalDateTime.now());
 
         if(type == Types.WARNING) {
-            String text = date + mainPrefix + RESET +" [" + YELLOW + "WARNING" + RESET + "]: " + message;
+            String text = "[" + date + "] " + mainPrefix + RESET +" [" + YELLOW + "WARNING" + RESET + "]: " + message;
             System.out.println(text);
         } else if(type == Types.INFO) {
-            String text = date + mainPrefix + RESET +" [" + BLUE + "INFO" + RESET + "]:" + message;
+            String text = "[" + date + "] " + mainPrefix + RESET +" [" + BLUE + "INFO" + RESET + "]:" + message;
             System.out.println(text);
         } else if(type == Types.ERROR) {
-            String text = date + mainPrefix + RESET +" [" + RED + "ERROR" + RESET + "]: " + message;
+            String text = "[" + date + "] " + mainPrefix + RESET +" [" + RED + "ERROR" + RESET + "]: " + message;
             System.out.println(text);
         } else if(type == Types.SUCCESS) {
-            String text = date + mainPrefix + RESET +" [" + GREEN + "SUCCESS" + RESET + "]: " + message;
+            String text = "[" + date + "] " + mainPrefix + RESET +" [" + GREEN + "SUCCESS" + RESET + "]: " + message;
             System.out.println(text);
         }
     }
